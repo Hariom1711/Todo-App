bd7e8cacb731618855abc7ded7008cfc529b6325
Hariom1711 <2003om1711@gmail.com>
2022-07-11 16:25:17 +0530

    update

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -98,7 +98,7 @@ useEffect(()=>{
 
 
     <figure  classname="bg-img">
-
+<figcaption></figcaption>
    <img src="https://cdn.icon-icons.com/icons2/1918/PNG/512/iconfinder-document03-1622833_121957.png" alt="" />
       
         <figcaption>Add Your List Here ✌ </figcaption>
